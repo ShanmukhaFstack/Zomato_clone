a1a44d89569da6ca6c651dd76b371e8d1f5a9f54
ShanmukhaFstack <shanmukha.fullstack@gmail.com>
2025-05-15 12:03:21 +0530

    jsdemo

---
+++ jsdemo.js
@@ -0,0 +1,55 @@
+//comments we are going to use
+// var age = 10;
+// var age = 20;
+
+let contactdetails = ("sai");
+// sai is a string needs to be in double quotes
+
+console.log(contactdetails); //print sai
+
+/*console.log(contactdetails);*/
+
+ let age = 10;
+ let pi = 3.14
+
+ console.log(age); //numbers
+
+let m = "Vankadari Sai";
+console.log(m); // string
+
+let bool= true;
+console.log(bool); // boolean
+
+let country;
+console.log(country); //undefined
+
+let empty = null;
+console.log(empty); //null
+
+let sym = Symbol('unique');
+console.log(sym);  //symbols
+
+// Var keyword
+var n = 5;
+console.log(n);
+
+var n = 20; // reassigning and redeclared is allowed
+console.log(n);
+
+// let keyword
+
+let productitems = 10;
+    productitems = 20; //here reassigned is allowed and didn't redeclared
+
+console.log(productitems);
+
+let productitems1 = 10;
+let productitems2 = 20; //here didn't reassigned with same name and redeclared
+
+console.log(productitems1);
+console.log(productitems2);
+
+
+// const keyword
+const num = 200;
+console.log(num); //Here cannnot redeclared and reassigned
