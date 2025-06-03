fa07a72d280d77e26f7ca791a58ac801513b87b6
ShanmukhaFstack <shanmukha.fullstack@gmail.com>
2025-06-03 17:44:26 +0530

    it is a switch case statements

---
+++ switch.js
@@ -0,0 +1,17 @@
+let fruit= "apple"
+switch (fruit) {
+    case "apple":
+        console.log("it is an apple");
+        break;
+          case "banana":
+        console.log("it is an banana");
+        break;
+          case "Mango":
+        console.log("it is an Mango");
+        break;
+        case "ice apple":
+        console.log("it is an ice apple");
+        break;
+        default:
+        console.log("not an fruit ");
+}
