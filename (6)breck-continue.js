5d03dd51b0fc694798d7ef0269904f6f60c9aa60
ShanmukhaFstack <shanmukha.fullstack@gmail.com>
2025-06-04 11:59:29 +0530

    break-continue

---
+++ (6)breck-continue.js
@@ -0,0 +1,11 @@
+// 🔧 Loop Control: break, continue
+//  break – exits loop immediately
+for (let i = 1; i <= 10; i++) {
+  if (i === 5) break;
+  console.log(i);
+}
+// continue – skips current iteration
+for (let i = 1; i <= 5; i++) {
+  if (i === 3) continue;
+  console.log(i);
+}
