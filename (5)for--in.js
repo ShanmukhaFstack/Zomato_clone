b510925759028dc48b8b547cf46e5ccdf4dcbd18
ShanmukhaFstack <shanmukha.fullstack@gmail.com>
2025-06-04 11:53:46 +0530

    add extra data

--- (5)for--in.js
+++ (5)for--in.js
@@ -3,25 +3,25 @@
 //   // code block
 // }
 // 5.1 Print object values
-let user = {name: "Alice", age: 25, city: "Delhi"};
+let user = {name: "Sai", age: 24, city: "tadipatri"};
 for (let key in user) {
   console.log(`${key}: ${user[key]}`);
 }
 // 5.2 Count keys in object
-let laptop = {brand: "HP", RAM: "16GB", SSD: "512GB"};
+let laptop = {brand: "HP PAVILION", RAM: "16GB", SSD: "512GB"};
 let count = 0;
 for (let prop in laptop) {
   count++;
 }
 console.log("Total Properties:", count);
 // 5.3 Capitalize values
-let student = {name: "bob", subject: "math"};
+let student = {name: "JANA", subject: "math"};
 for (let key in student) {
   student[key] = student[key].toUpperCase();
 }
 console.log(student);
 // 5.4 Validate all fields are filled
-let form = {name: "John", email: "john@gmail.com", phone: ""};
+let form = {name: "Shanmukha", email: "shanmukha.fullstack@gmail.com", phone: "IQOO"};
 let isValid = true;
 for (let key in form) {
   if (!form[key]) isValid = false;
