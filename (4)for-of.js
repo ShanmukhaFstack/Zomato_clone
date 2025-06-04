b2e92b80bbe377bd4e6c631481bbbd6e20dd5d69
ShanmukhaFstack <shanmukha.fullstack@gmail.com>
2025-06-04 11:39:43 +0530

    add more information

--- (4)for-of.js
+++ (4)for-of.js
@@ -3,7 +3,7 @@
 //   // code block
 // }
 // 4.1 Print each character in a name
-let name = "David";
+let name = "Shanmukha";
 for (let char of name) {
   console.log(char);
 }
@@ -15,7 +15,7 @@ for (let score of scores) {
 }
 console.log("Total:", total);
 // 4.3 Greet every city
-let cities = ["Delhi", "Mumbai", "Pune"];
+let cities = ["Delhi", "Mumbai", "Pune"," Bangalore"," Hyderabad"];
 for (let city of cities) {
   console.log(`Welcome to ${city}`);
 }
